Added average height method and fixes

Tetris_Board.py
@@ -1,20 +1,40 @@
-import numpy
+import numpy as np
+from Tetris_Piece import Piece
 
 class Board:
 
-	def __init__(self):
-		self.board = numpy.zeros(shape=(20, 10))
+	def __init__(self, state = []):
+
+		if state == []:
+			self.board_state = np.zeros(shape=(20, 10))
+		else:
+			self.board_state = state
 
 
 	def possible_moves(self, piece):
 		piece.print()
 
+	def get_avg_height(self):
+		h = [0,0,0,0,0,0,0,0,0,0]
+		for (x,y), value in np.ndenumerate(self.board_state):
+			if value == 1 and h[y] == 0:
+				h[y] = 20-x
+		return sum(h) / len(h)
+
+	def heuristic(self):
+		pass
+
+
+	def update_board(self):
+		for i, row in enumerate(self.board_state):
+			if 0 not in row:
+				np.delete()
 
 	def print_board(self):
-		print(self.board)
+		print(self.board_state)
 
 	def update_board(self, new_board):
-		self.board = new_board
+		self.board_state = new_board
 
 
 

ai.py
@@ -1,17 +1,43 @@
 #testing
-
+import numpy as np
 from tree import Tree, Node
 from Tetris_Board import Board
-from Tetris_Piece impor Piece
+from Tetris_Piece import Piece
 
-#
-root = Node([], 1)
 
+b = np.matrix([
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,1,0,0,0,0,0,0,0],
+[0,0,1,0,0,0,0,0,0,0],
+[0,0,1,0,0,0,0,0,0,0]
+])
+
+
+#initialisation
+board = Board(b)
+root = Node([], 1, board)
 tree = Tree(root)
 
-x = Board()
 
-x.print_board()
+board.print_board()
 
-y = Piece(tetris_shape[1])
-x.possible_moves(y)
+print(board.get_avg_height())
+
+#y = Piece(tetris_shape[1])
+#x.possible_moves(y)